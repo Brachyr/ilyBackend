dd2f4f338b8d2fc07f8a51a1bcc84afa0af74bfc
Raul Savianu <raul.savianu@tba.group>
2020-06-20 20:10:45 +0300

    cross origin + fix for problem finding in case there's no running test

--- LTIEminens/src/main/java/com/orsolyazolcsak/allamvizsga/controller/ProblemController.java
+++ LTIEminens/src/main/java/com/orsolyazolcsak/allamvizsga/controller/ProblemController.java
@@ -1,47 +1,58 @@
 package com.orsolyazolcsak.allamvizsga.controller;
 
-import com.orsolyazolcsak.allamvizsga.model.Problem;
-import com.orsolyazolcsak.allamvizsga.model.Test;
-import com.orsolyazolcsak.allamvizsga.repository.ProblemRepository;
-import com.orsolyazolcsak.allamvizsga.service.ProblemService;
-import com.orsolyazolcsak.allamvizsga.service.TestService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.orsolyazolcsak.allamvizsga.model.Problem;
+import com.orsolyazolcsak.allamvizsga.model.Test;
+import com.orsolyazolcsak.allamvizsga.service.ProblemService;
+import com.orsolyazolcsak.allamvizsga.service.TestService;
 
 @RestController
 @RequestMapping("/problem")
 public class ProblemController {
 
-    @Autowired
-    private ProblemService problemService;
-    
-    @Autowired
-    private TestService testService;
+  @Autowired
+  private ProblemService problemService;
 
-    @GetMapping
-    public List<Problem> getTests() {
-        return problemService.getAllProblemsByTest(testService.findRunning().map(Test::getId).orElse(null));
+  @Autowired
+  private TestService testService;
+
+  @CrossOrigin(origins = "http://localhost:3000")
+  @GetMapping
+  public List<Problem> getTests() {
+    Optional<Test> running = this.testService.findRunning();
+    if (running.isPresent()) {
+      return this.problemService.getAllProblemsByTest(running.get().getId());
     }
+    return new ArrayList<>();
+  }
 
-    @PostMapping
-    Problem newProblem(@RequestBody Problem newProblem) {
-        return problemService.createNewProblem(newProblem);
-    }
+  @PostMapping
+  Problem newProblem(@RequestBody Problem newProblem) {
+    return this.problemService.createNewProblem(newProblem);
+  }
 
-    @GetMapping("/{id}")
-    Optional<Problem> one(@PathVariable Long id) {
+  @GetMapping("/{id}")
+  Optional<Problem> one(@PathVariable Long id) {
 
-        return problemService.findById(id);
-        //.orElseThrow(()-> new ProblemNotFoundException(id));
-    }
+    return this.problemService.findById(id);
+    // .orElseThrow(()-> new ProblemNotFoundException(id));
+  }
 
-    @DeleteMapping("/{id}")
-    void deleteProblem(@PathVariable Long id) {
-        problemService.deleteById(id);
-    }
+  @DeleteMapping("/{id}")
+  void deleteProblem(@PathVariable Long id) {
+    this.problemService.deleteById(id);
+  }
 }
